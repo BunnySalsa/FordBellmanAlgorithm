dc208ff84fec05896dddfdf11d17cb887ece30fa
Artem Melihov <bunnysalsa4d@gmail.com>
2022-11-23 16:32:55 +0300

    Correcting collections

--- src/main/java/org/rsreu/FordBellman.java
+++ src/main/java/org/rsreu/FordBellman.java
@@ -15,28 +15,28 @@ public class FordBellman {
         int vertCount = graph.getAdjList().length; //Определяется кол-во вершин
         int[][] adjList = graph.getAdjList();
         int k = 0;//Порядок итерации
-        List<Integer> src;
-        List<Integer>[] shortestWays = new ArrayList[vertCount];
-        shortestWays[k] = new ArrayList<>();
+        Integer[] src;
+        Integer[][] shortestWays = new Integer[vertCount][];
+        shortestWays[k] = new Integer[vertCount];
         for (int i = 0; i < vertCount; i++) {//Заполнение путей бесконечностями
-            shortestWays[k].add(INFINITY);
+            shortestWays[k][i] = INFINITY;
         }
-        shortestWays[k].set(start, 0);//Установка кратчайшего пути для начальной точки
+        shortestWays[k][start] = 0;//Установка кратчайшего пути для начальной точки
         for (int i = 0; i < adjList[start].length; i++) {//Нулевая итерация. Установка первых кратчайших путей из начальной вершины
-            shortestWays[k].set(adjList[start][i], graph.getWeight(start, adjList[0][i]));
+            shortestWays[k][adjList[start][i]] = graph.getWeight(start, adjList[0][i]);
         }
-        src = Arrays.stream(adjList[start]).boxed().collect(Collectors.toList());//формирование множества S
+        src = Arrays.stream(adjList[start]).boxed().toArray(Integer[]::new);//формирование множества S
         while (k < vertCount - 1) {//Пока кол-во итераций меньше кол-ва вершин
-            shortestWays[k + 1] = new ArrayList<>(shortestWays[k]);
+            shortestWays[k + 1] = shortestWays[k].clone();
             for (Integer s : src) {//Для каждой вершины в множестве S
                 for (int j = 0; j < adjList[s].length; j++) {//Для каждой дуги из вершины множества S
-                    shortestWays[k + 1].set(adjList[s][j], Integer.min(shortestWays[k + 1].get(adjList[s][j]),
-                            shortestWays[k + 1].get(s) + graph.getWeight(s, adjList[s][j])));//Присвоение минимального пути
+                    shortestWays[k + 1][adjList[s][j]] = Integer.min(shortestWays[k + 1][adjList[s][j]],
+                            shortestWays[k + 1][s] + graph.getWeight(s, adjList[s][j]));//Присвоение минимального пути
                 }
             }
             int countOfMatch = 0;//Проверка условия окончания
             for (int i = 0; i < vertCount; i++) {//Подсчет минимальных путей, значение которых после итерации не изменилось
-                if(shortestWays[k].get(i).equals(shortestWays[k + 1].get(i))) countOfMatch++;
+                if(shortestWays[k][i].equals(shortestWays[k + 1][i])) countOfMatch++;
             }
             if (k <= vertCount - 1 && countOfMatch == vertCount) {
                 break;//Получен оптимальный результат
@@ -48,11 +48,11 @@ public class FordBellman {
             for (Integer s : src) {
                 newSrc.addAll(Arrays.stream(adjList[s]).boxed().collect(Collectors.toList()));
             }
-            src = newSrc.stream().distinct().collect(Collectors.toList());
+            src = newSrc.stream().distinct().toArray(Integer[]::new);
             k++;//Инкремент
         }
-        int[] result = new int[shortestWays[k].size()];//Перевод списка в массив integer
-        for (int i = 0; i < vertCount; i++) result[i] = shortestWays[k].get(i);
+        int[] result = new int[shortestWays[k].length];//Перевод списка в массив integer
+        for (int i = 0; i < vertCount; i++) result[i] = shortestWays[k][i];
         return result;
     }
 
